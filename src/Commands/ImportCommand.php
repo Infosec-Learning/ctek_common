3f62bb3d01efd79fa4064c3e2828dec4a342d00f
Patrick Wilson <pwilson@centretek.com>
2019-09-23 18:30:33 +0000

    generic reliability

--- src/Commands/ImportCommand.php
+++ src/Commands/ImportCommand.php
@@ -8,13 +8,16 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\Render\RenderContext;
 use Drupal\Core\Render\RendererInterface;
+use Drupal\ctek_common\ReliabilityHelper;
 use Drupal\node\NodeInterface;
 use Drupal\taxonomy\TermInterface;
 use Drush\Commands\DrushCommands;
 use GuzzleHttp\ClientInterface;
+use phpseclib\Net\SFTP;
 use Stiphle\Throttle\TimeWindow;
 use STS\Backoff\Backoff;
 use STS\Backoff\Strategies\PolynomialStrategy;
+use Symfony\Component\Console\Style\OutputStyle;
 
 abstract class ImportCommand extends DrushCommands {
 
@@ -55,11 +58,11 @@ abstract class ImportCommand extends DrushCommands {
 
   protected function doImport(callable $callback) {
     $io = $this->io();
-    $io->success("Beginning import...");
+    $io->success('Beginning import...');
     $transaction = $this->database->startTransaction();
     try {
       $this->renderer->executeInRenderContext(new RenderContext(), $callback);
-      $io->success("Import complete.");
+      $io->success('Import complete.');
     } catch (\Throwable $e) {
       $io->error($e->getMessage());
       $io->error($e->getTraceAsString());
@@ -69,43 +72,85 @@ abstract class ImportCommand extends DrushCommands {
 
   /**
    * @param string|\Psr\Http\Message\UriInterface $url
-   * @param $options
+   * @param array $options
+   *
+   * @param int $maxAttempts
+   * @param int $limit
+   * @param int $milliseconds
    *
    * @return \Psr\Http\Message\ResponseInterface
-   * @throws \Exception
    */
-  protected function httpGetReliably($url, array $options = [], $maxAttempts = 5, $limit = 0, $milliseconds = 0) {
+  protected function httpRequest($url, array $options = [], $maxAttempts = 5, $limit = 0, $milliseconds = 0) {
     $io = $this->io();
     $isVerbose = $io->isVerbose();
     $client = \Drupal::httpClient();
     if ($isVerbose) {
       $io->text("Calling $url...");
     }
-    $this->throttle($url, $limit, $milliseconds);
-    $backoffStrategy = new PolynomialStrategy(max(500, $milliseconds), 2);
-    $backoff = new Backoff($maxAttempts, $backoffStrategy);
-    $backoff->setErrorHandler(function(\Exception $exception, $attempt, $maxAttempts) use ($io, $url, $backoffStrategy) {
-      $io->warning("Attempt $attempt of $maxAttempts to call $url failed ({$exception->getMessage()}), retrying in {$backoffStrategy->getWaitTime($attempt)}ms...");
-    });
-    $result = $backoff->run(function() use ($client, $url, $options) {
+    if ($maxAttempts < 1) {
+      throw new \InvalidArgumentException('$maxAttempts must be greater than or equal to 1');
+    }
+    $helper = new ReliabilityHelper(function() use ($client, $url, $options, $io) {
       return $client->get($url, $options);
     });
+    if ($limit > 0 && $milliseconds > 0) {
+      $helper->throttle($url, $limit, $milliseconds, function ($wait, OutputStyle $io) {
+        if ($this->isVerbose()) {
+          $io->text("Throttle engaged, waiting $wait milliseconds...");
+        }
+      });
+    }
+    $helper->tolerateFaults(
+      $maxAttempts,
+      max(500, $milliseconds),
+      2,
+      function (\Exception $exception, $attempt, $maxAttempts, $waitTime) use ($io, $url) {
+        $io->warning("Attempt $attempt of $maxAttempts to call $url failed ({$exception->getMessage()}), retrying in {$waitTime}ms...");
+      }
+    );
+    $result = $helper->execute();
     if ($isVerbose) {
       $io->text("Got response from $url.");
     }
     return $result;
   }
 
-  protected function throttle($key, $limit = 0, $milliseconds = 0) {
-    if ($limit > 0 && $milliseconds > 0) {
-      $wait = $this->timeWindow->getEstimate($key, $limit, $milliseconds);
-      if ($wait > 0) {
-        if ($this->isVerbose()) {
-          $this->io()->text("Trottle engaged, waiting $wait milliseconds...");
-        }
-        $this->timeWindow->throttle($key, $limit, $milliseconds);
-      }
+  protected function sftpRequest($host, $port, $user, $password, $path, $maxAttempts = 5, $limit = 0, $milliseconds = 0) {
+    $io = $this->io();
+    $isVerbose = $io->isVerbose();
+    if ($maxAttempts < 1) {
+      throw new \InvalidArgumentException('$maxAttempts must be greater than or equal to 1');
     }
+    $helper = new ReliabilityHelper(function() use ($host, $port, $user, $path, $password, $io, $isVerbose) {
+      $sftp = new SFTP($host, $port);
+      if (!$sftp->login($user, $password)) {
+        throw new \Exception('Unable to log in to SFTP server.');
+      }
+      if ($isVerbose) {
+        $io->text('Logged in to SFTP server.');
+      }
+      $tempFilename = 'temporary://' . basename($path);
+      if (!$sftp->get($path, $tempFilename)) {
+        throw new \Exception('Unable to download file.');
+      }
+      return $tempFilename;
+    });
+    if ($limit > 0 && $milliseconds > 0) {
+      $helper->throttle("$host-$port-$user-$path", $limit, $milliseconds, function ($wait, OutputStyle $io) {
+        if ($this->isVerbose()) {
+          $io->text("Throttle engaged, waiting $wait milliseconds...");
+        }
+      });
+    }
+    $helper->tolerateFaults(
+      $maxAttempts,
+      max(500, $milliseconds),
+      2,
+      function (\Exception $exception, $attempt, $maxAttempts, $waitTime) use ($io, $path) {
+        $io->warning("Attempt $attempt of $maxAttempts to download file $path failed ({$exception->getMessage()}), retrying in {$waitTime}ms...");
+      }
+    );
+    return $helper->execute();
   }
 
   protected function conditionallyCreateTaxonomyTerm($vid, $termName) {
---
+++ src/ReliabilityHelper.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\ctek_common;
+
+use Stiphle\Throttle\TimeWindow;
+use STS\Backoff\Backoff;
+use STS\Backoff\Strategies\PolynomialStrategy;
+
+class ReliabilityHelper {
+
+  protected $timeWindow;
+
+  /**
+   * @var callable
+   */
+  protected $callback;
+
+  protected $throttle;
+
+  protected $key;
+
+  protected $maxExecutions;
+
+  protected $timePeriodMilliseconds;
+
+  protected $waitCallback;
+
+  protected $backoff;
+
+  protected $backoffMaxAttempts;
+
+  protected $backoffMilliseconds;
+
+  protected $backoffDegree;
+
+  protected $backoffErrorHandler;
+
+  public function __construct(callable $callback) {
+    $this->callback = $callback;
+    $this->timeWindow = new TimeWindow();
+  }
+
+  public function throttle($key, $maxExecutions, $timePeriodMilliseconds, callable $waitCallback = NULL) {
+    $this->throttle = TRUE;
+    $this->maxExecutions = $maxExecutions;
+    $this->timePeriodMilliseconds = $timePeriodMilliseconds;
+    $this->waitCallback = $waitCallback;
+    return $this;
+  }
+
+  public function tolerateFaults($maxAttempts = 5, $milliseconds = 500, $degree = 2, callable $errorHandler = NULL) {
+    $this->backoff = TRUE;
+    $this->backoffMaxAttempts = $maxAttempts;
+    $this->backoffMilliseconds = $milliseconds;
+    $this->backoffDegree = $degree;
+    $this->backoffErrorHandler = $errorHandler;
+    return $this;
+  }
+
+  public function execute() {
+    if ($this->throttle) {
+      $wait = $this->timeWindow->getEstimate($this->key, $this->maxExecutions, $this->timePeriodMilliseconds);
+      if ($wait > 0 && $this->waitCallback) {
+        ($this->waitCallback)($wait);
+      }
+      $this->timeWindow->throttle($this->key, $this->maxExecutions, $this->timePeriodMilliseconds);
+    }
+    if ($this->backoff) {
+      $backoffStrategy = new PolynomialStrategy($this->backoffMilliseconds, $this->backoffDegree);
+      $backoff = new Backoff($this->backoffMaxAttempts, $backoffStrategy);
+      if ($this->backoffErrorHandler) {
+        $backoff->setErrorHandler(function(\Exception $exception, $attempt, $maxAttempts) use ($backoffStrategy) {
+          ($this->backoffErrorHandler)($exception, $attempt, $maxAttempts, $backoffStrategy->getWaitTime($attempt));
+        });
+      }
+      return $backoff->run($this->callback);
+    } else {
+      return ($this->callback)();
+    }
+  }
+
+}
