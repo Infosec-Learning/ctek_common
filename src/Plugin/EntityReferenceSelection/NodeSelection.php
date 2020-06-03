107fded45716ca6fb3d921330cb3a806297e68ac
Patrick Wilson <pwilson@centretek.com>
2020-06-03 12:02:53 -0400

    wtf

--- src/Plugin/EntityReferenceSelection/NodeSelection.php
+++ src/Plugin/EntityReferenceSelection/NodeSelection.php
@@ -2,44 +2,6 @@
 
 namespace Drupal\ctek_common\Plugin\EntityReferenceSelection;
 
-use Drupal\Core\Database\Query\SelectInterface;
-use Drupal\Core\Entity\Annotation\EntityReferenceSelection;
-use Drupal\Core\Entity\Query\QueryInterface;
-use Drupal\node\Plugin\EntityReferenceSelection\NodeSelection as BaseNodeSelection;
-
-class NodeSelection extends BaseNodeSelection {
-
-  protected $match;
-
-  protected $matchOperator;
-
-  protected function buildEntityQuery($match = NULL, $match_operator = 'CONTAINS') : QueryInterface {
-    $this->match = $match;
-    $this->matchOperator = $match_operator;
-    return parent::buildEntityQuery($match, $match_operator);
-  }
-
-  public function entityQueryAlter(SelectInterface $query) : void {
-    $query->range(0, 20);
-    if ($this->matchOperator === 'CONTAINS') {
-      $sort = <<<EOSQL
-CASE WHEN LOWER(node_field_data.title) = LOWER(:keywords) THEN 1
-ELSE 2
-END
-EOSQL;
-      $query->addExpression($sort, 'exactMatchSort', [':keywords' => $this->match]);
-      $query->orderBy('exactMatchSort');
-      $query->groupBy('exactMatchSort');
-      $query->orderBy('node_field_data.title');
-      $query->groupBy('node_field_data.title');
-      $query->groupBy('base_table.vid');
-      $query->groupBy('base_table.nid');
-    }
-    parent::entityQueryAlter($query);
-  }
-
-namespace Drupal\ctek_common\Plugin\EntityReferenceSelection;
-
   use Drupal\Core\Database\Query\SelectInterface;
   use Drupal\Core\Entity\Annotation\EntityReferenceSelection;
   use Drupal\Core\Entity\Query\QueryInterface;
@@ -97,8 +59,5 @@ EOSQL;
 
     return $result;
   }
-
-}
-
 
 }
